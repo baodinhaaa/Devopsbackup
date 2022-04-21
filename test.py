7212cf0e97137c9896c5aafe780863520e9cc14e
Hung <phamsyhung2110@gmail.com>
2022-04-21 10:53:40 +0700

    push all 31/4/2022

---
+++ test.py
@@ -0,0 +1,4 @@
+value_list = [nginx_config_mod, env, app_name, action, find, 
+            replace_key,replace_value,php_upstream, nodejs_upstream,
+            cobr_upstream,set,env]  
+print(value_list[1])
